c5aad4b5e8445f1b636838450681d8883115ef8b
maru44 <takumaru22244@gmail.com>
2021-03-06 20:54:58 +0900

    test2

--- project/filebackends.py
+++ project/filebackends.py
@@ -1,6 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 # not allow overwrite of same file
+#aa
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False

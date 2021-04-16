bee5df625aaee78031e4e7be02f8783b65c2bdc7
maru44 <takumaru22244@gmail.com>
2021-04-16 22:36:13 +0900

    com out api_urls

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,25 +1,27 @@
 from django.urls import path
 from . import api_views as api
 
-app_name = 'api'
+app_name = "api"
 urlpatterns = [
-    # wanted list post 
-    path('wanted/', api.WantedAPI.as_view(), name="wanted_api"),
+    # wanted list post
+    path("wanted/", api.WantedAPI.as_view(), name="wanted_api"),
     # wanted detail update delete
-    path('wanted/<wanted_slug>/', api.WantedDetailAPI.as_view(), name="wanted_detail_api"),
+    path(
+        "wanted/<wanted_slug>/", api.WantedDetailAPI.as_view(), name="wanted_detail_api"
+    ),
     # user wanted list post
-    path('wanted/u/<username>/', api.WantedUsersAPI.as_view()),
+    path("wanted/u/<username>/", api.WantedUsersAPI.as_view()),
     # offer or change wanted offer
-    path('offering/<wanted_slug>/', api.OfferingAPI.as_view(), name='offering'),
+    path("offering/<wanted_slug>/", api.OfferingAPI.as_view(), name="offering"),
     # scraping
-    path('scrape/', api.scrape_api, name="scrape"),
-    path('batch/<wanted_slug>/', api.batch_offer, name='offer_batch'),
+    path("scrape/", api.scrape_api, name="scrape"),
+    # path('batch/<wanted_slug>/', api.batch_offer, name='offer_batch'),
     # give csrftoken
-    path('csrf/', api.csrf, name="csrf"),
+    path("csrf/", api.csrf, name="csrf"),
     # change is_gotten
-    path('gotten/<wanted_slug>', api.gotten_change),
+    path("gotten/<wanted_slug>", api.gotten_change),
     # test
-    path('test/', api.test),
+    path("test/", api.test),
     # inquiry
-    path('inquiry/', api.inquiry),
+    path("inquiry/", api.inquiry),
 ]
